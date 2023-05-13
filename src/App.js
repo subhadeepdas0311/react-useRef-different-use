c63fc9e8e11504b393147028cb45c6d76ca3d911
subhadeepdas0311 <subhadeepdas0311@gmail.com>
2023-05-13 19:51:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Time from './components/Time';
+
+export default function App() {
+    return (
+        <div className="app">
+            <div>
+                <Time />
+            </div>
+        </div>
+    );
+}
